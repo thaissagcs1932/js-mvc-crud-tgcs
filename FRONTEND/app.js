e73c8b288ac37f7f3b1ed21e9e7f5814b7d3ad5c
thaissagcs1932 <thaissagiulianecsilva@gmail.com>
2023-11-29 19:44:30 -0300

    jogo da velha alteracoes

--- FRONTEND/app.js
+++ FRONTEND/app.js
@@ -1,3 +1,22 @@
 import { controller } from "./src/controller.js";
 
 controller.run();
+import { useState } from 'react';
+
+function Square({ value, onSquareClick }) {
+  return (
+    <button className="square" onClick={onSquareClick}>
+      {value}
+    </button>
+  );
+}
+
+function Board({ xIsNext, squares, onPlay }) 
+  function handleClick(i) 
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    const nextSquares = squares.slice();
+    export default function Square() {
+        return <button className="square">X</button>;
+      }
